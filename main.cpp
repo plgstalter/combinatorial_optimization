0f2691c851241544d2c16dabb1377c0a946e2718
plgstalter <pierrelouisgstalter67@gmail.com>
2022-01-01 19:40:49 +0100

    first

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+#include "Graph.h"
+
+template <typename T>
+void vector_debug(std::vector<T> vec) {
+	int j = 0;
+	for (int i=0; i<size(vec); i++) {
+		std::cout << j << std::endl;
+		j++;
+	}
+}
+
+void vector_printer(std::vector<int> vec) {
+	for (int i=0; i<size(vec); i++) {
+		std::cout <<  vec[i];
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main(int argc, char const *argv[]) { 
+	int m = 1;
+	std::vector< std::vector<int> > nodes = {
+		{1, 2},
+		{0, 4},
+		{0, 3, 4},
+		{2},
+		{1, 2}
+	};
+	std::vector< std::vector<int> > weights = { 
+		{10, 20},
+		{10, 40},
+		{20, 8, 500},
+		{8},
+		{40, 500}
+	};
+	Graph Instance(nodes, weights);
+	if ( argc < 2) {
+		Instance.Dijkstra(m);
+	} else {
+		Instance.Dijkstra(atoi(argv[1]));
+	}
+	vector_printer(Instance.predecessors);
+ return 0;
+ }
