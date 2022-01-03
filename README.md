8e11f872cf7de4d5266f02480617149e38771243
plgstalter <pierrelouisgstalter67@gmail.com>
2022-01-03 13:35:51 +0100

    nice generator

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ School project for the class "Combinatorial Optimization" at the University of M
 ## Goal
 
 Implementing Dijkstra's algorithm on different data structures so as to compare experimental complexity. Thus, it will be compared to theoretical complexity.
+
+### Random Graphs generator
+
+To best test the algorithm's implementation, it will run on lots of graphs, randomly generated. Speed doesn't matter as much for these graphs, so they'll be computed in `Python`, using `numpy`.
+
+To compute `n` graphs of `m` nodes, type:
+
+	python random_graphs.py n m | tr '[' '{' | tr ']' '}' > graphs.txt
+
+The graphs will be stored in the file `graphs.txt` in such a form that is easily readable by `c++` code.
+
+__Remark:__ The pipes after the python call require a Unix system. On Windows, find another way to change the '[]' into '{}'.
--- random_graphs.py
+++ random_graphs.py
@@ -1,13 +1,30 @@
 import numpy as np
 from sys import argv
 
-def random_graph(number_of_graphs, graph_max_nodes=100, density=.6):
+def random_graph(number_of_graphs, size_nodes, density=.3):
 
-    choice_array = np.arange(graph_max_nodes**2)
+    res = []
+    choice_array = np.arange(size_nodes**2)
     for i in range(number_of_graphs):
         np.random.shuffle(choice_array)
-        arcs = choice_array[:int(np.floor(density * graph_max_nodes))]
-        print(arcs)        
+        arcs = choice_array[:int(np.floor(density * size_nodes**2))]
+        res.append(arcs)
+    return res
 
-if  len(argv) > 1:
-    random_graph(int(argv[1]))
+def decode_graph(Arcs, size_nodes):
+    '''
+    put graph in proper arcs form
+    '''
+    for arcs in Arcs:
+        start = arcs//size_nodes
+        end = arcs%size_nodes
+        print('{')
+        for i in range(size_nodes):
+            print(start[(end == i) & (start != end)], end=',')
+            print()
+        print('}')
+        print()
+
+if  len(argv) > 2:
+    Arcs = random_graph(int(argv[1]), int(argv[2]))
+    decode_graph(Arcs, int(argv[2]))
