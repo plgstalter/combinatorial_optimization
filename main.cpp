ce3d8181adb813abfda52db7c5397f22ca6f096c
plgstalter <pierrelouisgstalter67@gmail.com>
2022-01-07 19:19:22 +0100

    standard graph (adjacency matrix) algo as well

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #include "Graph.h"
 #include "BinaryHeap.h"
+#include "std_graph.h"
 #include "test_graphs.hpp"
 
 template <typename T>
@@ -23,20 +24,26 @@ void vector_printer(std::vector<int> vec) {
 }
 
 int main(int argc, char const *argv[]) { 
-	if (argc > 2) {
-		test_graphs teste;
-		int argv1 = atoi(argv[1]);
-		int argv2 = atoi(argv[2]);
-		if (argv1 < size(teste.arcs) && argv2 < size(*teste.arcs[0])) {
-			Graph test_graph(*teste.arcs[argv1], *teste.weights[argv1]);
-			test_graph.Dijkstra(argv2);
-			vector_printer(test_graph.predecessors);
-		} else {
-			std::cerr << "Please indicate a number smaller than the amount of graphs.\n Same for the starting vertex.\n";
+	int n = 1;
+	if (n == 1) {
+		if (argc > 2) {
+			test_graphs teste;
+			int argv1 = atoi(argv[1]);
+			int argv2 = atoi(argv[2]);
+			if (argv1 < size(teste.arcs) && argv2 < size(*teste.arcs[0])) {
+				Graph test_graph(*teste.arcs[argv1], *teste.weights[argv1]);
+				test_graph.Dijkstra(argv2);
+				vector_printer(test_graph.predecessors);
+			} else {
+				std::cerr << "Please indicate a number smaller than the amount of graphs.\n Same for the starting vertex.\n";
+		}
+		       
+		}	else {
+			std::cerr << "Please which graph you want to run Dijkstra on, and the starting vertex.\n";
+		}
+	} else if (n == 2) {
+		BinaryHeap Test(3);
+		std::cout <<  Test.size << std::endl;
 	}
-	       
-	}	else {
-		std::cerr << "Please which graph you want to run Dijkstra on, and the starting vertex.\n";
-	};
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,10 +1,10 @@
 CC = g++
 CFLAGS = -g -Wall -std=c++17
 
-main: main.o Graph.o BinaryHeap.o
+main: main.o Graph.o BinaryHeap.o std_graph.o
 	$(CC) -o main *.o
 
-main.o: main.cpp Graph.h BinaryHeap.h test_graphs.hpp
+main.o: main.cpp Graph.h BinaryHeap.h std_graph.h test_graphs.hpp
 	$(CC) $(CFLAGS) -c main.cpp
 
 Graph.o: Graph.cpp
@@ -13,5 +13,8 @@ Graph.o: Graph.cpp
 BinaryHeap.o: BinaryHeap.cpp
 	$(CC) $(CFLAGS) -c BinaryHeap.cpp
 
-clean: Graph.o BinaryHeap.o main.o  
+std_graph.o: std_graph.cpp
+	$(CC) $(CFLAGS) -c std_graph.cpp
+
+clean: Graph.o BinaryHeap.o main.o std_graph.o 
 	rm *.o
