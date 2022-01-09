a8ac59d5a28f53edb7da0416e3364e4023c54886
plgstalter <pierrelouisgstalter67@gmail.com>
2022-01-09 18:13:10 +0100

    multiple calls

--- main.cpp
+++ main.cpp
@@ -25,8 +25,20 @@ void vector_printer(std::vector<int> vec) {
 
 int main(int argc, char const *argv[]) { 
 	int n = 1;
+	int m = 1;
 	if (n == 1) {
-		if (argc > 2) {
+		if (m == 1 && argc > 1) {
+			test_graphs teste;
+			Graph test(*teste.arcs[0], *teste.weights[0]);
+			int argv1 = atoi(argv[1]);
+			for (int i=0; i<80; i++) {
+				test  = Graph(*teste.arcs[i], *teste.weights[i]);
+				for (int j=0; j<10; j++)
+				test.Dijkstra(10 * j + argv1);
+				vector_printer(test.predecessors);
+				std::cout << "\n";
+			}
+		} else if (argc > 2) {
 			test_graphs teste;
 			int argv1 = atoi(argv[1]);
 			int argv2 = atoi(argv[2]);
@@ -42,8 +54,32 @@ int main(int argc, char const *argv[]) {
 			std::cerr << "Please which graph you want to run Dijkstra on, and the starting vertex.\n";
 		}
 	} else if (n == 2) {
-		BinaryHeap Test(3);
-		std::cout <<  Test.size << std::endl;
+		BinaryHeap Test(5);
+	} else if (n == 3) {
+		int Size = 5;
+		std::vector<int> Arcs = {
+		0, 0, 1, 1, 0,
+		0, 0, 0, 0, 1,
+		1, 0, 0, 1, 0,
+		1, 0, 1, 0, 0,
+		0, 1, 0, 0, 0
+		};
+		std::vector<int> Weights = {
+		0, 0, 20, 100, 0,
+		0, 0, 0, 0, 50,
+		20, 0, 0, 230, 0,
+		100, 0, 230, 0, 0,
+		0, 50, 0, 0, 0
+		};
+		std_graph TestGraph(Size, Arcs, Weights);
+		TestGraph.Dijkstra(0);
+		for (int i=0; i<Size*Size; i++) {
+			if (i%Size == 0) {
+				std::cout << "\n";
+			}
+			std::cout << TestGraph.weights[i] << " ";
+		}
+		std::cout << "\n";
 	}
 	return 0;
-}
+};
